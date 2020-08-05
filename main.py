92790deca2fa7df196d19b8af46c8b978c28bdbb
thejduman <thejduman@yahoo.com>
2020-08-05 15:40:14 +0000

    made bounce function for ball, starting on paddle collision detection

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ class Paddle(object):
     if key[pygame.K_UP]:
       self.y -= 1
 
+  def hit(self, obj):
+    if obj.x + 25 <= self.x and 
+
+
 class Wall(object):
   def __init__(self):
     pygame.sprite.Sprite.__init__(self)
@@ -40,6 +44,7 @@ class Wall(object):
     self.y = y
     self.hitbox = (x, y, width + 5, height + 5)
 
+
 class Ball():
   def __init__(self):
     pygame.sprite.Sprite.__init__(self)
@@ -47,25 +52,39 @@ class Ball():
     self.image = Ball.image
     self.image = pygame.transform.scale(self.image,(50, 50))
     self.x = 100
-    self.y = 100
+    self.y = 150
     self.x_velocity = 1
     self.y_velocity = 1
     self.hitbox = (self.x, self.y, 55, 55)
+
+  def draw(self, surface):
+    surface.blit(self.image, (self.x, self.y))
   
-  def bounce(self):
+  def bounce(self, screen_height, screen_width):
     self.x += self.x_velocity
     self.y += self.y_velocity
     
+    #left side
+    if self.x <= 0:
+      self.x_velocity = 1
+    #right side
+    if self.x >= screen_width - 50:
+      self.x_velocity = -1
+    #top of screen
+    if self.y <= 0:
+      self.y_velocity = 1
+    if self.y >= screen_height - 50:
+      self.y_velocity = -1
 
-    
 
 
 pygame.init()
-screen_width = 600
+screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((int(screen_width),int(screen_height)))
 p = Paddle()
 w = Wall()
+b = Ball()
 
 running = True
 while running:
@@ -77,7 +96,9 @@ while running:
   screen.fill((255, 255, 255))
   p.draw(screen)
   p.movement()
-  w.draw(screen, 600, 25, 0, 0)
-  w.draw(screen, 25, 600, 575, 0)
-  w.draw(screen, 600, 25, 0, 575)
+  w.draw(screen, 800, 25, 0, 0)
+  w.draw(screen, 25, 800, 775, 0)
+  w.draw(screen, 800, 25, 0, 775)
+  b.draw(screen)
+  b.bounce(screen_height, screen_width)
   pygame.display.update()
